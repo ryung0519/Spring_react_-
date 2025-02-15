e12311327d46f99823ba9e9be8428ba9215d0559
ryung0519 <happy79891@gmail.com>
2025-02-15 17:39:59 +0900

    ~p127

---
+++ IdeaProjects/boot-developer/src/main/java/me/ryung/bootdeveloper/SpringBootDevelopertApplication.java
@@ -0,0 +1,11 @@
+package me.ryung.bootdeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDevelopertApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDevelopertApplication.class, args);
+    }
+}
